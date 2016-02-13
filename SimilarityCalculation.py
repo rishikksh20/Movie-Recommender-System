ae0eb7d28a9aa7e1d31047b19e3e1edd067670e7
rishikksh20 <rishikksh20@gmail.com>
2016-02-13 06:16:59 +0530

    make a user-movie table for analysis

--- SimilarityCalculation.py
+++ SimilarityCalculation.py
@@ -1,1 +1,15 @@
 import pandas as pd
+
+
+r_cols=['user_id','movie_id','rating']
+ratings=pd.read_csv('ml-latest/ratings.csv',names=r_cols,usecols=range(3))
+ratings.head()
+m_cols=['movie_id','title']
+movies=pd.read_csv('ml-latest/movies.csv',names=m_cols,usecols=range(2))
+movies.head()
+ratings=pd.merge(movies,ratings)
+ratings.head()
+movieRatings=ratings.pivot_table(index=['user_id'],columns=['title'],values='rating',aggfunc = lambda x: x)
+movieRatings.head()
+starWarsRating= movieRatings["Round Midnight (1986)"]
+starWarsRating.head()
