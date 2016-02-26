8bd749fa77ff5261ab637c535fcf6d0cffd0862d
rishikksh20 <rishikksh20@gmail.com>
2016-02-26 14:52:22 +0530

    movie Recommender System based on User-item and Item-item based collaborative filtering

---
+++ movieRecommender.py
@@ -0,0 +1,313 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 14:18:08 2016
+
+@author: rishikesh
+"""
+
+
+import numpy as np
+import pandas as pd
+
+names = ['user_id', 'item_id', 'rating', 'timestamp']
+df =pd.read_csv('ml-100k/u.data', sep='\t', names=names)
+df.head()
+
+
+n_users = df.user_id.unique().shape[0]
+n_items = df.item_id.unique().shape[0]
+print str(n_users) + ' users'
+print str(n_items) + ' items'
+
+ratings = np.zeros((n_users, n_items))
+for row in df.itertuples():
+    ratings[row[1]-1, row[2]-1] = row[3]
+ratings
+
+sparsity = float(len(ratings.nonzero()[0]))
+sparsity /= (ratings.shape[0] * ratings.shape[1])
+sparsity *= 100
+print 'Sparsity: {:4.2f}%'.format(sparsity)
+
+def train_test_split(ratings):
+    test = np.zeros(ratings.shape)
+    train = ratings.copy()
+    for user in xrange(ratings.shape[0]):
+        test_ratings = np.random.choice(ratings[0, :].nonzero()[0], 
+                                        size=10, 
+                                        replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings] = ratings[user, test_ratings]
+        
+    # Test and training are truly disjoint
+    assert(np.all((train * test) == 0)) 
+    return train, test
+
+train, test = train_test_split(ratings)
+
+
+def slow_similarity(ratings, kind='user'):
+    if kind == 'user':
+        axmax = 0
+        axmin = 1
+    elif kind == 'item':
+        axmax = 1
+        axmin = 0
+    sim = np.zeros((ratings.shape[axmax], ratings.shape[axmax]))
+    for u in xrange(ratings.shape[axmax]):
+        for uprime in xrange(ratings.shape[axmax]):
+            rui_sqrd = 0.
+            ruprimei_sqrd = 0.
+            for i in xrange(ratings.shape[axmin]):
+                sim[u, uprime] = ratings[u, i] * ratings[uprime, i]
+                rui_sqrd += ratings[u, i] ** 2
+                ruprimei_sqrd += ratings[uprime, i] ** 2
+            sim[u, uprime] /= rui_sqrd * ruprimei_sqrd
+    return sim
+
+def fast_similarity(ratings, kind='user'):
+    if kind == 'user':
+        sim = ratings.dot(ratings.T)
+    elif kind == 'item':
+        sim = ratings.T.dot(ratings)
+    norms = np.array([np.sqrt(np.diagonal(sim))])
+    return sim / norms / norms.T
+   
+#%timeit slow_user_similarity(train)
+fast_similarity(train, kind='user')
+
+user_similarity = fast_similarity(train, kind='user')
+item_similarity = fast_similarity(train, kind='item')
+print item_similarity[:4, :4]
+
+def predict_slow_simple(ratings, similarity, kind='user'):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        for i in xrange(ratings.shape[0]):
+            for j in xrange(ratings.shape[1]):
+                pred[i, j] = similarity[i, :].dot(ratings[:, j])\
+                             /np.sum(np.abs(similarity[i, :]))
+        return pred
+    elif kind == 'item':
+        for i in xrange(ratings.shape[0]):
+            for j in xrange(ratings.shape[1]):
+                pred[i, j] = similarity[j, :].dot(ratings[i, :].T)\
+                             /np.sum(np.abs(similarity[j, :]))
+
+        return pred
+
+def predict_fast_simple(ratings, similarity, kind='user'):
+    if kind == 'user':
+        return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif kind == 'item':
+        return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+        
+from sklearn.metrics import mean_squared_error
+
+def get_mse(pred, actual):
+    # Ignore nonzero terms.
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+
+item_prediction = predict_fast_simple(train, item_similarity, kind='item')
+user_prediction = predict_fast_simple(train, user_similarity, kind='user')
+
+print 'User-based CF MSE: ' + str(get_mse(user_prediction, test))
+print 'Item-based CF MSE: ' + str(get_mse(item_prediction, test))
+
+def predict_topk(ratings, similarity, kind='user', k=40):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        for i in xrange(ratings.shape[0]):
+            top_k_users = [np.argsort(similarity[:,i])[:-k-1:-1]]
+            for j in xrange(ratings.shape[1]):
+                pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users]) 
+                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+    if kind == 'item':
+        for j in xrange(ratings.shape[1]):
+            top_k_items = [np.argsort(similarity[:,j])[:-k-1:-1]]
+            for i in xrange(ratings.shape[0]):
+                pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T) 
+                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))        
+    
+    return pred
+    
+pred = predict_topk(train, user_similarity, kind='user', k=40)
+print 'Top-k User-based CF MSE: ' + str(get_mse(pred, test))
+
+pred = predict_topk(train, item_similarity, kind='item', k=40)
+print 'Top-k Item-based CF MSE: ' + str(get_mse(pred, test))
+
+k_array = [5, 15, 30, 50, 100, 200]
+user_train_mse = []
+user_test_mse = []
+item_test_mse = []
+item_train_mse = []
+
+def get_mse(pred, actual):
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+
+for k in k_array:
+    user_pred = predict_topk(train, user_similarity, kind='user', k=k)
+    item_pred = predict_topk(train, item_similarity, kind='item', k=k)
+    
+    user_train_mse += [get_mse(user_pred, train)]
+    user_test_mse += [get_mse(user_pred, test)]
+    
+    item_train_mse += [get_mse(item_pred, train)]
+    item_test_mse += [get_mse(item_pred, test)]  
+    
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+pal = sns.color_palette("Set2", 2)
+
+plt.figure(figsize=(8, 8))
+plt.plot(k_array, user_train_mse, c=pal[0], label='User-based train', alpha=0.5, linewidth=5)
+plt.plot(k_array, user_test_mse, c=pal[0], label='User-based test', linewidth=5)
+plt.plot(k_array, item_train_mse, c=pal[1], label='Item-based train', alpha=0.5, linewidth=5)
+plt.plot(k_array, item_test_mse, c=pal[1], label='Item-based test', linewidth=5)
+plt.legend(loc='best', fontsize=20)
+plt.xticks(fontsize=16);
+plt.yticks(fontsize=16);
+plt.xlabel('k', fontsize=30);
+plt.ylabel('MSE', fontsize=30);
+
+
+
+
+
+def predict_nobias(ratings, similarity, kind='user'):
+    if kind == 'user':
+        user_bias = ratings.mean(axis=1)
+        ratings = (ratings - user_bias[:, np.newaxis]).copy()
+        pred = similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+        pred += user_bias[:, np.newaxis]
+    elif kind == 'item':
+        item_bias = ratings.mean(axis=0)
+        ratings = (ratings - item_bias[np.newaxis, :]).copy()
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+        pred += item_bias[np.newaxis, :]
+        
+    return pred
+    
+user_pred = predict_nobias(train, user_similarity, kind='user')
+print 'Bias-subtracted User-based CF MSE: ' + str(get_mse(user_pred, test))
+
+item_pred = predict_nobias(train, item_similarity, kind='item')
+print 'Bias-subtracted Item-based CF MSE: ' + str(get_mse(item_pred, test))
+
+
+def predict_topk_nobias(ratings, similarity, kind='user', k=40):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        user_bias = ratings.mean(axis=1)
+        ratings = (ratings - user_bias[:, np.newaxis]).copy()
+        for i in xrange(ratings.shape[0]):
+            top_k_users = [np.argsort(similarity[:,i])[:-k-1:-1]]
+            for j in xrange(ratings.shape[1]):
+                pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users]) 
+                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+        pred += user_bias[:, np.newaxis]
+    if kind == 'item':
+        item_bias = ratings.mean(axis=0)
+        ratings = (ratings - item_bias[np.newaxis, :]).copy()
+        for j in xrange(ratings.shape[1]):
+            top_k_items = [np.argsort(similarity[:,j])[:-k-1:-1]]
+            for i in xrange(ratings.shape[0]):
+                pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T) 
+                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items])) 
+        pred += item_bias[np.newaxis, :]
+        
+    return pred
+
+k_array = [5, 15, 30, 50, 100, 200]
+user_train_mse = []
+user_test_mse = []
+item_test_mse = []
+item_train_mse = []
+
+for k in k_array:
+    user_pred = predict_topk_nobias(train, user_similarity, kind='user', k=k)
+    item_pred = predict_topk_nobias(train, item_similarity, kind='item', k=k)
+    
+    user_train_mse += [get_mse(user_pred, train)]
+    user_test_mse += [get_mse(user_pred, test)]
+    
+    item_train_mse += [get_mse(item_pred, train)]
+    item_test_mse += [get_mse(item_pred, test)]  
+    
+pal = sns.color_palette("Set2", 2)
+
+plt.figure(figsize=(8, 8))
+plt.plot(k_array, user_train_mse, c=pal[0], label='User-based train', alpha=0.5, linewidth=5)
+plt.plot(k_array, user_test_mse, c=pal[0], label='User-based test', linewidth=5)
+plt.plot(k_array, item_train_mse, c=pal[1], label='Item-based train', alpha=0.5, linewidth=5)
+plt.plot(k_array, item_test_mse, c=pal[1], label='Item-based test', linewidth=5)
+plt.legend(loc='best', fontsize=20)
+plt.xticks(fontsize=16);
+plt.yticks(fontsize=16);
+plt.xlabel('k', fontsize=30);
+plt.ylabel('MSE', fontsize=30);
+
+
+
+import requests
+import json
+
+response = requests.get('http://us.imdb.com/M/title-exact?Toy%20Story%20(1995)')
+print response.url.split('/')[-2]
+
+# Get base url filepath structure. w185 corresponds to size of movie poster.
+headers = {'Accept': 'application/json'}
+payload = {'api_key': 'INSERT API_KEY HERE'} 
+response = requests.get("http://api.themoviedb.org/3/configuration", params=payload, headers=headers)
+response = json.loads(response.text)
+base_url = response['images']['base_url'] + 'w185'
+
+def get_poster(imdb_url, base_url):
+    # Get IMDB movie ID
+    response = requests.get(imdb_url)
+    movie_id = response.url.split('/')[-2]
+    
+    # Query themoviedb.org API for movie poster path.
+    movie_url = 'http://api.themoviedb.org/3/movie/{:}/images'.format(movie_id)
+    headers = {'Accept': 'application/json'}
+    payload = {'api_key': 'INSERT API_KEY HERE'} 
+    response = requests.get(movie_url, params=payload, headers=headers)
+    try:
+        file_path = json.loads(response.text)['posters'][0]['file_path']
+    except:
+        # IMDB movie ID is sometimes no good. Need to get correct one.
+        movie_title = imdb_url.split('?')[-1].split('(')[0]
+        payload['query'] = movie_title
+        response = requests.get('http://api.themoviedb.org/3/search/movie', params=payload, headers=headers)
+        movie_id = json.loads(response.text)['results'][0]['id']
+        payload.pop('query', None)
+        movie_url = 'http://api.themoviedb.org/3/movie/{:}/images'.format(movie_id)
+        response = requests.get(movie_url, params=payload, headers=headers)
+        file_path = json.loads(response.text)['posters'][0]['file_path']
+        
+    return base_url + file_path
+    
+    
+toy_story = 'http://us.imdb.com/M/title-exact?Toy%20Story%20(1995)'
+
+# Load in movie data
+idx_to_movie = {}
+with open('u.item', 'r') as f:
+    for line in f.readlines():
+        info = line.split('|')
+        idx_to_movie[int(info[0])-1] = info[4]
+        
+def top_k_movies(similarity, mapper, movie_idx, k=6):
+    return [mapper[x] for x in np.argsort(similarity[movie_idx,:])[:-k-1:-1]]
+    
+idx = 0 # Toy Story
+movies = top_k_movies(item_similarity, idx_to_movie, idx)
